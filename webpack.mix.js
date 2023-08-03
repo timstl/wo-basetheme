f34f7831bb85f91741adc87f8b4af39509bd3021
Tim Gieseking <timstl@gmail.com>
2023-08-03 12:54:30 -0500

    update webpack to allow for separate .js files within blocks

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,6 +4,7 @@ mix
 	.sourceMaps(false, "source-map")
 	.webpackConfig({ devtool: "source-map" })
 	.js("src/js/**/*.js", "dist/js/theme.js")
+	.js("template-parts/blocks/**/*.js", "dist/js/blocks/")
 	.sass("src/scss/style.scss", "dist/css/")
 	.sass("src/scss/editor-styles.scss", "dist/css/")
 	.options({
